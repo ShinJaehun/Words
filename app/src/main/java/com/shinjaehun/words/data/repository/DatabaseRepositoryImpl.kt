8e6e0bca9a3d1692e87a0fc4efc26901f74db442
ShinJaehun <gunsmoke.shin@gmail.com>
2024-11-02 15:22:22 +0900

    FB를 통해 자료를 주고 받기 때문에 coroutine context가 필요

--- app/src/main/java/com/shinjaehun/words/data/repository/DatabaseRepositoryImpl.kt
+++ app/src/main/java/com/shinjaehun/words/data/repository/DatabaseRepositoryImpl.kt
@@ -10,26 +10,37 @@ import kotlinx.coroutines.withContext
 class DatabaseRepositoryImpl(
     private val wordsDao: WordsDao
 ) : DatabaseRepository {
-    override suspend fun getListOfWords(): LiveData<List<WordEntry>>  =
+    override suspend fun getListOfWords(): LiveData<List<WordEntry>> =
         withContext(Dispatchers.IO) {
             return@withContext wordsDao.getWordsList()
         }
 
-    override suspend fun deleteAll()= wordsDao.deleteAll()
+    override suspend fun deleteAll() =
+        withContext(Dispatchers.IO) {
+            wordsDao.deleteAll()
+        }
 
-    override suspend fun addWord(word: WordEntry) = wordsDao.insert(word)
-
-    override suspend fun deleteWord(word: WordEntry, isDelete: Boolean) {
-        if (isDelete || FirebaseUtil.uid == null)
-            wordsDao.deleteWord(word.word)
-        else
+    override suspend fun addWord(word: WordEntry) =
+        withContext(Dispatchers.IO) {
             wordsDao.insert(word)
-//        wordsDao.deleteWord(word.word)
-    }
+        }
+
+    override suspend fun deleteWord(word: WordEntry, isDelete: Boolean) =
+        withContext(Dispatchers.IO) {
+            if (isDelete || FirebaseUtil.uid == null)
+                wordsDao.deleteWord(word.word)
+            else
+                wordsDao.insert(word) // 근데 왜 delete()가 아닌 insert()?????
+            //        wordsDao.deleteWord(word.word)
+        }
 
     override suspend fun getListFromFirebase(): LiveData<List<WordEntry>> =
-        FirebaseUtil.liveDataList
+        withContext(Dispatchers.IO) {
+            FirebaseUtil.liveDataList
+        }
 
     override suspend fun addListOfWords(words: List<WordEntry>) =
-        wordsDao.insertList(words)
+        withContext(Dispatchers.IO) {
+            wordsDao.insertList(words)
+        }
 }
